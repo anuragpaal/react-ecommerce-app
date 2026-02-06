565c79b46bf09bb3dc3e90a6adc8e81052f3e9bf
Anurag <anurag8731@gmail.com>
2026-02-06 11:37:03 +0530

    Day 4 - Quantity management added

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,40 @@ function App() {
     { id: 4, name: "Headphones", price: 1500 },
   ]);
 
-  const [cart,setCart] = useState([]);
+  const [cart, setCart] = useState([]);
 
+  // Add Cart
   const addToCart = (product) => {
-    setCart([...cart,product]);
-  }
+    const exist = cart.find((item) => item.id == product.id);
 
-  const removeFromCart = (id) => {
-    setCart(cart.filter(item => item.id !== id))
-  }
+    if (exist) {
+      setCart(
+        cart.map((item) =>
+          item.id === product.id ? { ...item, qty: item.qty + 1 } : item,
+        ),
+      );
+    } else {
+      setCart([...cart, { ...product, qty: 1 }]);
+    }
+  };
+
+  // Increase Qty
+  const increaseQty = (id) => {
+    setCart(
+      cart.map((item) =>
+        item.id === id ? { ...item, qty: item.qty + 1 } : item,
+      ),
+    );
+  };
+
+  // Decerase Qty
+  const decreaseQty = (id) => {
+    setCart(
+      cart.map((item) =>
+        item.id === id && item.qty > 1 ? { ...item, qty: item.qty - 1 } : item,
+      ),
+    );
+  };
 
   return (
     <div style={{ padding: 20 }}>
@@ -40,10 +65,9 @@ function App() {
       <h3>Cart</h3>
       {cart.map((item) => (
         <div key={item.id}>
-          {item.name} - {item.price}
-          <button onClick={()=> removeFromCart(item.id)}>
-            Remove
-          </button>
+          {item.name} - {item.price} * {item.qty}
+          <button onClick={() => increaseQty(item.id)}>+</button>
+          <button onClick={() => decreaseQty(item.id)}>-</button>
         </div>
       ))}
     </div>

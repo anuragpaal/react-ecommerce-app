751dad7593fde90d07c4aceaf23689dc1c99e9f8
Anurag <anurag8731@gmail.com>
2026-02-06 11:11:00 +0530

    Day 2 - Add to cart logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
-import { useState } from "react"
+import { useState } from "react";
 
 function App() {
   const [products] = useState([
-     { id: 1, name: "Shoes", price: 2000 },
+    { id: 1, name: "Shoes", price: 2000 },
     { id: 2, name: "T-Shirt", price: 800 },
     { id: 3, name: "Watch", price: 5000 },
     { id: 4, name: "Headphones", price: 1500 },
-  ])
+  ]);
+
+  const [cart,setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart([...cart,product]);
+  }
 
   return (
-    <div style={{padding : 20}}>
+    <div style={{ padding: 20 }}>
       <h1>E-Commerce App</h1>
 
-      {products.map((product)=> (
+      <h2>Cart Items : {cart.length}</h2>
+
+      {products.map((product) => (
         <div key={product.id}>
-            <h3>{product.name}</h3>
-            <p>{product.price}</p>
+          <h3>{product.name}</h3>
+          <p>{product.price}</p>
+
+          <button onClick={() => addToCart(product)}>Add to Cart</button>
         </div>
       ))}
     </div>
-  )
+  );
 }
 
 export default App;
